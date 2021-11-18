c006c76f128700cfbc5055698ee941e744b073d7
Patrick <kimanthip@gmail.com>
2021-11-18 08:09:04 +0300

    Added user authorization middleware

--- src/models/user/User.model.js
+++ src/models/user/User.model.js
@@ -48,4 +48,26 @@ const storeUserRefreshJWT = (_id, token) => {
   });
 };
 
-module.exports = { insertUser, getUserByEmail, storeUserRefreshJWT };
+const getUserById = (_id) => {
+  return new Promise((resolve, reject) => {
+    if (!_id) return false;
+
+    try {
+      UserSchema.findOne({ _id }, (error, data) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(data);
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+module.exports = {
+  insertUser,
+  getUserByEmail,
+  storeUserRefreshJWT,
+  getUserById,
+};
